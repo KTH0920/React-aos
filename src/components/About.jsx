7cab3fc642b963d5fa890cf1459caad34976361d
KTH0920 <taehyun9482@gmail.com>
2025-08-07 15:09:05 +0900

    aos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style/About.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const Aboutme = () => {
   return (
     <section>
       <div className="t-wrap">
-        <h2>aboutme title</h2>
-        <p>
+        <h2 data-aos="fade-up">aboutme title</h2>
+        <p data-aos="fade-up" data-aos-delay="50">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis,
           voluptas!
         </p>
